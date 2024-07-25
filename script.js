8f663927e808ee97fc27a62e9b60952f42d7a065
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-07-24 19:51:43 -0700

    Remove the colors and use a border container

--- script.js
+++ script.js
@@ -11,12 +11,6 @@ for (let i = 0; i < GRID_SIZE; i++){
     for (let j = 0; j < GRID_SIZE; j++){
         const square = document.createElement('div');
         square.setAttribute('class', 'grid');
-        if(j % 2 === 0){
-            square.style.backgroundColor = (i % 2) === 0 ? "black" : "yellow";
-        }
-        else{
-            square.style.backgroundColor = (i % 2) === 0 ? "yellow" : "black";
-        }
         square.style.height = SQUARE_SIZE.toString() + "px";
         square.style.width = SQUARE_SIZE.toString() + "px";
         CONTAINER.appendChild(square);
