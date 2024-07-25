e843b92f0aaad9ec02262a9927c50b0e54a67f43
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-07-24 21:28:24 -0700

    Make color to be random RGB

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ let gridSize = 16; // Squares per side of the grid, default 16
 CONTAINER.style.width = CONTAINER_SIZE.toString() + "px";
 CONTAINER.style.height = CONTAINER_SIZE.toString() + "px";
 
+/**
+ * Randomly generate an integer from 0 (inclusive) to max (exclusive).
+ * Not intended for max < 0.
+ *
+ * @param {number} max the max range (exclusive)
+ * @returns {number} randomly generated integer from 0 (inclusive) to max (exclusive).
+ */
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function createGrid(size) {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
@@ -34,7 +45,9 @@ button.addEventListener("click", () => {
 CONTAINER.addEventListener("mouseover", (e) => {
   targetElement = e.target;
   if (targetElement.getAttribute("class") === "grid") {
-    targetElement.style.backgroundColor = "aquamarine";
+    targetElement.style.backgroundColor = `rgb(${getRandomInt(
+      256
+    )}, ${getRandomInt(256)}, ${getRandomInt(256)})`;
   }
 });
 
